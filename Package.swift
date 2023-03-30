fce0725455e8074f743b7f9523246d1ce36b082e
Anton Malygin <toxaval@gmail.com>
2023-03-30 22:21:26 +0400

    Fixed Package.swift wrong path for tests

--- Package.swift
+++ Package.swift
@@ -26,7 +26,8 @@ let package = Package(
         ),
         .testTarget(
             name: "StableCollectionViewLayoutTests",
-            dependencies: ["StableCollectionViewLayout"]
+            dependencies: ["StableCollectionViewLayout"],
+            path: "StableCollectionViewLayoutTests"
         ),
     ]
 )
